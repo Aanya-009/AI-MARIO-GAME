98b8e43b72eec9da06d32706e73f2c0bdf74fb99
Aanya-009 <aanyag009@gmail.com>
2023-02-05 11:42:16 +0545

    added sounds

--- characters_environment.js
+++ characters_environment.js
@@ -222,6 +222,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    mario_coin.play();
   };
 }
     
@@ -311,6 +312,7 @@ function manualControl(character){
 function jumping(character){
 	if( (noseY < 200 && character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
+    mario_jump.play();
 	}
 }
 
@@ -360,6 +362,7 @@ function StepOnEnemy(obj1,obj2){
 		obj2.live=false;
     obj1.killing=30;
     obj1.kills++;
+    mario_kill.play();
     if(obj1.velocity.y>=gameConfig.jump*0.8){
       obj1.velocity.y=gameConfig.jump*0.8;
     }else{
@@ -377,6 +380,10 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+
+    if(character.liveNumber > 0){
+      mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -387,7 +394,8 @@ function checkStatus(character){
     reviveAfterMusic(character);
   }
   if(character.live==false && character.liveNumber==0){
-    gameConfig.status="gameover"
+    gameConfig.status="gameover";
+    mario_gameover.play();
   }
 
 }
